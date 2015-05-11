8bdda310fa1dc8353f7d25a0d0084d963bb6e66e
SirAlexiner <Sir_Alexiner@hotmail.com>
2015-05-11 19:05:57 +0200

    Added files
    added all ready existing files

---
+++ index.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Makesite</title>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script type="text/javascript" src="main.js"></script>
+	<link href="favicon.jpg" rel="shortcut icon" type="image/x-icon">
+    <link href="favicon.jpg" rel="icon" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+</body>
+</html>
